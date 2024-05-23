36a908bff0d719306586529e02e18d0d2e0cb86e
Kolya <you@example.com>
2024-05-23 19:42:39 +0300

    some mistakes

---
+++ strings_lesson_01.py
@@ -0,0 +1,18 @@
+student_name_1 = 'Alex'
+student_name_2 = 'Bob'
+
+print(id(student_name_1))
+print(id(student_name_2))
+
+print(student_name_1)
+print(' printed ')
+
+
+# do not perform like this
+# this is some info : some info
+
+
+
+
+# імя = "моє імя"
+print (імя)
